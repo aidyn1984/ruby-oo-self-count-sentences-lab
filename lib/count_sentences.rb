bc7dedb3d9d6da80976e0549d5d73f44041433ef
aidyn1984 <csa.aiden@gmail.com>
2021-03-17 20:48:04 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+  
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
+
+# "This is a string! It has three sentences. Right?".count_sentences
+# "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+
+# binding.pry
